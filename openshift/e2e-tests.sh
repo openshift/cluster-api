7cbd548e992499337f21eb51c59d378917196142
Christian Schlotter <christi.schlotter@gmail.com>
2025-10-07 13:58:51 +0200

    os-test-1-11-1-rebase

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -9,13 +9,14 @@ tmp="$(mktemp -d)"
 
 if [ "${PULL_BASE_REF}" == "master" ]; then
   # the default branch for cluster-capi-operator is main.
-  CCAPIO_BASE_REF="main"
+  echo "Building CCAPIO from pr-fix-conditions-set-lasttransitiontime-test"
+  CCAPIO_BASE_REF="pr-fix-conditions-set-lasttransitiontime-test"
 else
   CCAPIO_BASE_REF=$PULL_BASE_REF
 fi
 
-echo "cloning github.com/openshift/cluster-capi-operator at branch '$CCAPIO_BASE_REF'"
-git clone --single-branch --branch="$CCAPIO_BASE_REF" --depth=1 "https://github.com/openshift/cluster-capi-operator.git" "$tmp"
+echo "cloning github.com/chrischdi/cluster-capi-operator at branch '$CCAPIO_BASE_REF'"
+git clone --single-branch --branch="$CCAPIO_BASE_REF" --depth=1 "https://github.com/chrischdi/cluster-capi-operator.git" "$tmp"
 
 echo "running cluster-capi-operator's: make e2e"
 exec make -C "$tmp" e2e
